79374e910b474de70e2622742d6df8a8e46d0a99
Paweł Sobczak <github@fixeq.qzz.io>
2025-12-20 16:33:44 +0100

    Fix popup layout

--- webroot/app/components/UpdateConfirmPopup.vue
+++ webroot/app/components/UpdateConfirmPopup.vue
@@ -1,18 +1,19 @@
 <template>
-  <div
-    v-if="visible"
-    class="popup-overlay active"
-    @click="$emit('cancel')"
-  >
-    <div class="popup" @click.stop>
-      <h3>Update Chroot Environment</h3>
-      <p>This will apply any available updates to the chroot environment.</p>
-      <p>The chroot will be started if it's not running. Continue?</p>
-      <div class="buttons">
-        <button class="btn" @click="$emit('confirm')">
-          Yes
+  <div v-if="visible" class="popup-overlay active" @click="$emit('cancel')">
+    <div class="popup-content" @click.stop>
+      <div class="popup-header">
+        <h3>Update Chroot Environment</h3>
+        <button class="close-btn" title="Close" @click="$emit('cancel')">
+          ×
         </button>
-        <button class="btn" @click="$emit('cancel')">No</button>
+      </div>
+      <div class="popup-body">
+        <p>This will apply any available updates to the chroot environment.</p>
+        <p>The chroot will be started if it's not running. Continue?</p>
+        <div class="script-actions">
+          <button class="btn" @click="$emit('confirm')">Yes</button>
+          <button class="btn" @click="$emit('cancel')">No</button>
+        </div>
       </div>
     </div>
   </div>
